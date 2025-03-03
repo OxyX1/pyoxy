cc1fe90a864f292b15c425259617f3eaf88f2a28
oxyum services <29GariglioSe@fortcherry.org>
2025-03-03 16:46:37 -0500

    push

---
+++ pyoxy/main.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+class tools:
+    class console:
+        def write(string):
+            os.system(f'echo {string}')
+        def read(string):
+            input(string)
+        def exit():
+            sys.exit()
+    
+    class system:
+        pass
+    
+    class network:
+        pass
+    
+    class cloud:
+        def cloud_browser():
+            
+
+    class info:
+        pass
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyoxy",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="oxyus",
+    description="A python package built for mutlitools",
+    url="https://github.com/OxyX1/pyoxy.git",
+)
